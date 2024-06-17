2682603fca5145090d810c974d1bb233674f4df9
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2024-06-17 20:37:17 +0600

    added infinite scroll

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,31 +3,41 @@ import Card from "./Card";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [memes, setMemes] = useState(null);
+  const [memes, setMemes] = useState([]);
+  const [showShimmer, setShowShimmer] = useState(false);
 
   const fetchData = async () => {
+    setShowShimmer(true);
     const data = await fetch("https://meme-api.com/gimme/20");
     const res = await data.json();
     setTimeout(() => {
-      setMemes(res?.memes);
-    }, 4000);
+      setShowShimmer(false);
+      setMemes((memes) => [...memes, ...res.memes]);
+    }, 3000);
   };
 
   useEffect(() => {
     fetchData();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY + 1 >= document.body.scrollHeight) {
+      fetchData();
+    }
+  };
+
+  console.log({ showShimmer });
 
   return (
     <div>
-      {!memes ? (
-        <Shimmer />
-      ) : (
-        <div className="flex items-center flex-wrap justify-center gap-8">
-          {memes.map((meme, i) => (
-            <Card key={i} data={meme} />
-          ))}
-        </div>
-      )}
+      <div className="flex items-center flex-wrap justify-center gap-8">
+        {memes?.length > 0 &&
+          memes?.map((meme, i) => <Card key={i} data={meme} />)}
+
+        {showShimmer && <Shimmer />}
+      </div>
     </div>
   );
 };
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,18 +1,14 @@
 const Shimmer = () => {
-  return (
-    <div className="flex items-center flex-wrap justify-center gap-8">
-      {Array(20)
-        .fill(0)
-        .map((item, i) => (
-          <div
-            key={i}
-            className="w-72 animate-pulse border border-gray-400 rounded-xl p-5"
-          >
-            <div className="w-full bg-gray-300 rounded-xl h-60"></div>
-            <p className="bg-gray-300 w-full rounded-lg h-5 mt-2"></p>
-          </div>
-        ))}
-    </div>
-  );
+  return Array(20)
+    .fill(0)
+    .map((item, i) => (
+      <div
+        key={i}
+        className="w-72 animate-pulse border border-gray-400 rounded-xl p-5"
+      >
+        <div className="w-full bg-gray-300 rounded-xl h-60"></div>
+        <p className="bg-gray-300 w-full rounded-lg h-5 mt-2"></p>
+      </div>
+    ));
 };
 export default Shimmer;
